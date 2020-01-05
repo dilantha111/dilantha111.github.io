9edbe8e3cf954731a12563dd5c4034034d7d7c7a
dilantha111 <dilantha.prasanjith111@gmail.com>
2020-01-05 10:03:29 +0530

    Updates

---
+++ precache-manifest.9d2a08be0ef85164bfff1ace98fe6e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17479f059330d96751099ee4a0568f09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54597de72fbfd266c69c",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "753fc5caf1bc809967b9",
+    "url": "/static/js/2.51cab7f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.51cab7f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54597de72fbfd266c69c",
+    "url": "/static/js/main.1b104039.chunk.js"
+  },
+  {
+    "revision": "018d5e63d6d7ade2f311",
+    "url": "/static/js/runtime-main.e1f21542.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
